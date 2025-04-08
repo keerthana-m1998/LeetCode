6af0c4a7d8ac6b0be72cc3d3c9fc85caa7edd6bd
Keerthana M <71525892+keerthana-m1998@users.noreply.github.com>
2025-04-08 23:14:43 +0530

    Time: 27 ms (46.76%), Space: 18.4 MB (71.18%) - LeetHub

--- 1219-longest-well-performing-interval/1219-longest-well-performing-interval.py
+++ 1219-longest-well-performing-interval/1219-longest-well-performing-interval.py
@@ -1,13 +1,14 @@
-class Solution:
+class Solution: #TC: O(n) SP: O(n)
     def longestWPI(self, hours: List[int]) -> int:
+        # Prefix + HashMap
         res = score = 0
-        seen = {}
+        seen = {} # HashMap {score: first_occurence_indx}
         for i , h in enumerate(hours):
-            score += h>8
-            score -= h<9
-            if score >0:
-                res = i+1
-            seen.setdefault(score,i)
+            score += h>8 # score +=1 if hours[i]> 8 
+            score -= h<9 # score -=1 if hours[i]< 8 [1,1,-1,-1,-1,-1,9]
+            if score >0: # SubArray 
+                res = i+1 # Valid subarray from i, continous length will be updated in res  ## Greedy track to get max subarray
+            seen.setdefault(score,i) # if score not in seen (Record first occurence)
             if score -1 in seen:
                 res = max(res,i-seen[score-1])
         return res
