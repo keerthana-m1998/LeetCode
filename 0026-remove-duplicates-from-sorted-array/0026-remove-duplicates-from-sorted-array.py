6317624405c0d88ab50f0e40eeaaa71f709b8c1d
Keerthana M <71525892+keerthana-m1998@users.noreply.github.com>
2025-12-01 21:31:41 +0530

    Time: 3 ms (47.74%), Space: 18.8 MB (74.35%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -9,9 +9,9 @@ class Solution:
 
         # Hashset gives n(log N ) complexity
         # 2 pointer gives O(N)
-        i = 0
+        i = 0 
         for j in range(1,len(nums)):
             if nums[i]!=nums[j]:
                 nums[i+1]=nums[j]
                 i+=1
-        return i+1        
+        return i+1
