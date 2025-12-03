c0d494df636a0c9b58c0ede0da7edbb8fa4fee86
Keerthana M <71525892+keerthana-m1998@users.noreply.github.com>
2025-12-03 08:14:39 +0530

    Time: 1 ms (87.06%), Space: 19.2 MB (87.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,10 +1,20 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashmap = {}
-        counter,res = 0,0
+        # HAshmap uses extra memory O(n), O(n)
+        # hashmap = {}
+        # counter,res = 0,0
+        # for n in nums:
+        #     hashmap[n] = 1+hashmap.get(n,0)
+        #     res = n if hashmap[n]>counter else res
+        #     counter = max(hashmap[n],counter) 
+        # return res
+
+        #(Boyer-Moore Voting)
+        counter = 0
+        candidate = None
         for n in nums:
-            hashmap[n] = 1+hashmap.get(n,0)
-            res = n if hashmap[n]>counter else res
-            counter = max(hashmap[n],counter)
-        return res
+            if counter == 0:
+                candidate = n
+            counter += (1 if candidate == n else -1)
+        return candidate
         
