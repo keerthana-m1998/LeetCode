d918f4dc24ffdfac8b948b02089121ac1cb449fd
Keerthana M <71525892+keerthana-m1998@users.noreply.github.com>
2025-12-07 16:43:04 +0530

    Time: 0 ms (100%), Space: 17.6 MB (83.58%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,17 +1,45 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
-        nums.sort()
         n = len(nums)
+        freq = [0]*(n+1) 
+        # freq[i] = how many nums are exactly i (for i in [0, n])
         
-        # Try all possible x from 1 to n
-        for x in range(1, n + 1):
-            # index of the first element among the last x elements
-            idx = n - x
-            
-            # Check if there are at least x elements >= x
-            if nums[idx] >= x:
-                # If there is an element before idx, it must be < x
-                if idx == 0 or nums[idx - 1] < x:
-                    return x
-        
+        # any num > n is counted in freq[n]
+        for num in nums:
+            if num>=n:
+                freq[n]+=1
+            else:
+                freq[num]+=1  # num and freq[num] are same
+         # count_ge will track how many numbers are >= current x
+        counter = 0
+
+
+        # Try x from n down to 1
+        for x in range(n,0,-1):
+            counter += freq[x] # add numbers that are exactly x
+            if counter == x:
+                return x
+
         return -1
+
+
+# nums = [0, 4, 3, 0, 4]
+# n = 5
+
+# Build freq:
+
+# nums: 0,4,3,0,4
+
+# freq[0] = 2
+# freq[3] = 1
+# freq[4] = 2
+# others 0
+
+# Now check from x = 5 → 1:
+
+# x = 5: count_ge += freq[5] = 0 → count_ge = 0 ≠ 5
+# x = 4: count_ge += freq[4] = 2 → count_ge = 2 ≠ 4
+# x = 3: count_ge += freq[3] = 1 → count_ge = 3 ✅ count_ge == x → return 3
+# And indeed in nums, elements ≥ 3 are: [4,3,4] → 3 elements → x = 3 ✅
+
+# O(n),O(n)
