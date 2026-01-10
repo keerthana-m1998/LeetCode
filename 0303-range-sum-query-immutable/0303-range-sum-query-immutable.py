af7ab0c5b331e39ba0c7461418b8cb456b5f2d61
Keerthana M <71525892+keerthana-m1998@users.noreply.github.com>
2026-01-10 18:28:59 +0530

    Time: 5 ms (51.44%), Space: 22.8 MB (5.36%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,6 +1,8 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
+        # Build prefix sum array       
+        # prefix[i] = sum of nums[0] to nums[i-1]
         self.prefix = [0]
         for num in nums:
             self.prefix.append(self.prefix[-1] + num)
@@ -14,3 +16,18 @@ class NumArray:
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
+
+
+# Index:      0   1   2   3   4   5
+# nums:     [-2,  0,  3, -5,  2, -1]
+
+# prefix:  [0, -2, -2,  1, -4, -2, -3]
+#           ↑   ↑   ↑   ↑   ↑   ↑   ↑
+#           │   │   │   │   │   │   └─ sum of all 6 elements
+#           │   │   │   │   │   └─ sum of first 5 elements
+#           │   │   │   │   └─ sum of first 4 elements
+#           └─ sum of 0 elements (empty)
+
+# Query: sumRange(2, 5)
+# prefix[6] - prefix[2] = (-3) - (-2) = -1 ✓
+# O(N),O(N)
